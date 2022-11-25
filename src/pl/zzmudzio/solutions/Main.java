e19e019aaf160fca8915647399b36433450e318c
zzmudzio <zzmudzio@gmail.com>
2022-11-25 21:02:32 +0100

    next kata solution

--- src/pl/zzmudzio/solutions/Main.java
+++ src/pl/zzmudzio/solutions/Main.java
@@ -1,11 +1,11 @@
 package pl.zzmudzio.solutions;
 
-import pl.zzmudzio.solutions.prizeDrawKata.Rank;
 
+import pl.zzmudzio.solutions.NewCashierKata.Solution;
 
 public class Main {
     public static void main(String[] args) {
-        Rank.nthRank("Addison,Jayden,Sofia,Michael,Andrew,Lily,YLil", new Integer[]{4, 2, 1, 4, 3, 1, 1}, 1);
+        System.out.println(Solution.getOrder("pizzachickenfriesburgercokemilkshakefriessandwich"));
     }
 }
 
---
+++ src/pl/zzmudzio/solutions/NewCashierKata/Solution.java
@@ -0,0 +1,26 @@
+package pl.zzmudzio.solutions.NewCashierKata;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static String getOrder(String input) {
+        List<String> menu = Arrays.asList("burger", "fries", "chicken", "pizza", "sandwich", "onionrings",
+                "Milkshake", "Coke");
+        String correctedOrder = "";
+        for(String menuItem: menu) {
+            Pattern menuPosition = Pattern.compile(menuItem.toLowerCase());
+            Matcher match = menuPosition.matcher(input);
+            while(match.find()) {
+                correctedOrder = correctedOrder
+                        .concat(capitalize(input.substring(match.start(), match.end()))+" ");
+            }
+        }
+        return correctedOrder.trim();
+    }
+    public static String capitalize(String word) {
+        return String.valueOf(word.charAt(0)).toUpperCase() + word.substring(1);
+    }
+}
