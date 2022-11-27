4efb57ce13c88ee6273338b4afe4fc85c115b606
zzmudzio <zzmudzio@gmail.com>
2022-11-27 13:53:15 +0100

    added next kata solution

--- src/pl/zzmudzio/solutions/Main.java
+++ src/pl/zzmudzio/solutions/Main.java
@@ -1,11 +1,14 @@
 package pl.zzmudzio.solutions;
 
-
-import pl.zzmudzio.solutions.new_cashier_kata.Solution;
+import com.google.common.math.IntMath;
+import pl.zzmudzio.solutions.steps_in_primes_kata.StepInPrimes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.getOrder("pizzachickenfriesburgercokemilkshakefriessandwich"));
+        long[] result = StepInPrimes.step(2,10000000, 11000000);
+        for(long element : result) {
+            System.out.print(element);
+        }
     }
 }
 
---
+++ src/pl/zzmudzio/solutions/steps_in_primes_kata/StepInPrimes.java
@@ -0,0 +1,22 @@
+package pl.zzmudzio.solutions.steps_in_primes_kata;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import com.google.common.math.IntMath;
+
+
+public class StepInPrimes {
+    public static long[] step(int g, long m, long n) {
+        List<Integer> primeNumbers = IntStream.rangeClosed((int) m, (int) n)
+                .filter((number) -> IntMath.isPrime(number))
+                .boxed()
+                .toList();
+        for(int i=0; i<primeNumbers.size(); i++) {
+            for(int j=i+1; j<primeNumbers.size(); j++) {
+                if(primeNumbers.get(j) - primeNumbers.get(i) == g) return new long[]{primeNumbers.get(i),
+                primeNumbers.get(j)};
+            }
+        }
+        return null;
+    }
+}
